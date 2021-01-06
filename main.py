fa01b61a7a25b5da1dc22a32b1e148ae1ee9f2b6
김명기 <33053284+riroan@users.noreply.github.com>
2021-01-06 23:11:51 +0900

    1

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 from PyQt5 import uic
 
 form_class = uic.loadUiType("main.ui")[0]
@@ -8,8 +10,18 @@ class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.timer = QTimer(self)
+        self.timer.start(1000)
+        self.timer.timeout.connect(self.timeout)
         
-app = QApplication(sys.argv)
-mainWindow = WindowClass()
-mainWindow.show()
-app.exec_()
+    def timeout(self):
+        cur_time = QTime.currentTime()
+        str_time = cur_time.toString("hh:mm:ss")
+        self.statusBar().showMessage(str_time)
+        
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainWindow = WindowClass()
+    mainWindow.show()
+    app.exec_()
